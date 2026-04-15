649302eba5d784cfcb451ee9c29bb3eb184b6b3f
aciltas <ciltasaylin@gmail.com>
2026-04-14 21:51:26 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,8 +4,38 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
+
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
 # First example
 def add(a, b): 
-    pass
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return a / b
+
+def logarithim(a, b):
+    if a <= 0 or b <= 0 or b == 1:
+        raise ValueError
+    return math.log(a, b)
+
+def exponent(a, b):
+    return a ** b
+        
 
 
